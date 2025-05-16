d1a3de88765f0950e7d80d8703f91f7f1a95e798
Zizii2 <meglado20@gmail.com>
2025-05-16 16:23:38 +0200

    added layer of self sizing test editor from size of the terminal

--- kilo.c
+++ kilo.c
@@ -2,8 +2,9 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
-#include <termios.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 #include <unistd.h>
 
 /*** defines ***/
@@ -13,6 +14,9 @@
 
 struct editorConfig {
   struct termios orig_termios; // Saved copy of users ternimal before use
+	// Size of text editor
+	int screenrows;
+  int screencols;
 };
 struct editorConfig E;
 
@@ -55,10 +59,21 @@ char editorReadKey() {
   return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /*** output ***/
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+	for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -83,8 +98,13 @@ void editorProcessKeypress() {
 }
 
 /*** init ***/
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
 	enableRawMode();
+	initEditor();
 
 	while (1) {
 		editorRefreshScreen();
