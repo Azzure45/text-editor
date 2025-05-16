b6f3057db8d5949fc0ce6ccc3625bf1d5b269780
Zizii2 <meglado20@gmail.com>
2025-05-16 18:38:54 +0200

    removed the hard codes 'Hello World', and added code to read from a text file, but only a single line

--- kilo.c
+++ kilo.c
@@ -1,4 +1,8 @@
 /*** includes ***/
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -154,14 +158,23 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /*** file i/o ***/
-void editorOpen() {
-  char *line = "Hello, world!";
-  ssize_t linelen = 13;
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) { linelen--; }
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
 }
 
 /*** append buffer ***/
@@ -189,7 +202,7 @@ void editorDrawRows(struct abuf *ab) {
   int y;
 	for (y = 0; y < E.screenrows; y++) {
 		if (y >= E.numrows) {
-			if (y == E.screenrows / 3) {
+			if (E.numrows == 0 && y == E.screenrows / 3) {
 				char welcome[80];
 				int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
 				if (welcomelen > E.screencols) welcomelen = E.screencols;
@@ -296,14 +309,15 @@ void initEditor() {
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
-	enableRawMode();
-	initEditor();
-	editorOpen();
-
-	while (1) {
-		editorRefreshScreen();
-		editorProcessKeypress();
-	}
-	return 1;
+int main(int argc, char *argv[]) {
+  enableRawMode();
+  initEditor();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
+  while (1) {
+    editorRefreshScreen();
+    editorProcessKeypress();
+  }
+  return 0;
 }
