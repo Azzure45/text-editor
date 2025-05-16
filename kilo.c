2dcd93ca32a6de37a91cc56ea64d90ef932f661a
Zizii2 <meglado20@gmail.com>
2025-05-16 18:11:20 +0200

    added full side to side movement with HOME and END keys

--- kilo.c
+++ kilo.c
@@ -18,6 +18,8 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+	HOME_KEY,
+  END_KEY,
 	PAGE_UP,
   PAGE_DOWN, 
 };
@@ -82,8 +84,12 @@ int editorReadKey() {
 				if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
 				if (seq[2] == '~') {
 					switch (seq[1]) {
-						case '5': return PAGE_UP;
-						case '6': return PAGE_DOWN;
+						case '1': return HOME_KEY;
+            case '4': return END_KEY;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
 					}
 				}
 			} else {
@@ -93,9 +99,16 @@ int editorReadKey() {
 					case 'B': return ARROW_DOWN;
 					case 'C': return ARROW_RIGHT;
 					case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
 				}
 			}
-	}
+		} else if (seq[0] == 'O') {
+      switch (seq[1]) {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
+      }
+    }
     return '\x1b';
   } else { // Not an arrow key
     return c;
@@ -228,12 +241,17 @@ void editorProcessKeypress() {
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
 		break;
+	case HOME_KEY:
+		E.cx = 0;
+		break;
+	case END_KEY:
+		E.cx = E.screencols - 1;
+		break;
 	case PAGE_UP:
 	case PAGE_DOWN:
 		{
 			int times = E.screenrows;
-			while (times--)
-				editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			while (times--){ editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN); }
 		}
 		break;
 	case ARROW_UP:
