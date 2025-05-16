4cc104f1366a2fae819afa97f5847caa3f4c4817
Zizii2 <meglado20@gmail.com>
2025-05-16 15:22:23 +0200

    added time out to read() after wating 1/10 of a sec for an input

--- kilo.c
+++ kilo.c
@@ -7,36 +7,40 @@
 struct termios orig_termios; // Saved copy of users ternimal before use
 
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    
-    atexit(disableRawMode); //! For future refrence this is a very good function to know "atexit()"
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	
+	atexit(disableRawMode); //! For future refrence this is a very good function to know "atexit()"
 
-    struct termios raw = orig_termios;
-    tcgetattr(STDIN_FILENO, &raw);
+	struct termios raw = orig_termios;
+	tcgetattr(STDIN_FILENO, &raw);
 
-    //Turning off terminal flags
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    raw.c_oflag &= ~(OPOST);
-    raw.c_cflag |= (CS8);
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	//Turning off terminal flags
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
-    enableRawMode();
+	enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-    }
-    return 1;
+	while (1) {
+		char c = '\0';
+		read(STDIN_FILENO, &c, 1);
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+		if (c == 'q') break;
+	}
+	return 1;
 }
