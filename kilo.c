a04d1ad38634b45deaee9d5659a1ee47785ea22f
Zizii2 <meglado20@gmail.com>
2025-05-16 18:04:53 +0200

    added full page movement with page up/down keys

--- kilo.c
+++ kilo.c
@@ -17,7 +17,9 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN, 
+  ARROW_DOWN,
+	PAGE_UP,
+  PAGE_DOWN, 
 };
 
 /*** data ***/
@@ -76,14 +78,24 @@ int editorReadKey() {
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 		// Check if it is the start of an arrow key
     if (seq[0] == '[') {
-			// Translate from ABCD to ENUM
-      switch (seq[1]) {
-				case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
-      }
-    }
+			if (seq[1] >= '0' && seq[1] <= '9') {
+				if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+				if (seq[2] == '~') {
+					switch (seq[1]) {
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+					}
+				}
+			} else {
+				// Translate from ABCD to ENUM
+				switch (seq[1]) {
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+				}
+			}
+	}
     return '\x1b';
   } else { // Not an arrow key
     return c;
@@ -184,20 +196,28 @@ void editorRefreshScreen() {
 
 /*** input ***/
 void editorMoveCursor(int key) {
-  switch (key) {
+	switch (key) {
 	case ARROW_LEFT:
-		E.cx--;
+		if (E.cx != 0) {
+			E.cx--;
+		}
 		break;
 	case ARROW_RIGHT:
-		E.cx++;
+		if (E.cx != E.screencols - 1) {
+			E.cx++;
+		}
 		break;
 	case ARROW_UP:
-		E.cy--;
+		if (E.cy != 0) {
+			E.cy--;
+		}
 		break;
 	case ARROW_DOWN:
-		E.cy++;
+		if (E.cy != E.screenrows - 1) {
+			E.cy++;
+		}
 		break;
-  }
+}
 }
 
 void editorProcessKeypress() {
@@ -207,6 +227,14 @@ void editorProcessKeypress() {
 		write(STDOUT_FILENO, "\x1b[2J", 4);
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
+		break;
+	case PAGE_UP:
+	case PAGE_DOWN:
+		{
+			int times = E.screenrows;
+			while (times--)
+				editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+		}
 		break;
 	case ARROW_UP:
 	case ARROW_DOWN:
